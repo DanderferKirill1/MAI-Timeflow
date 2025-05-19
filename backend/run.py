47285863b1c9c7cc8b838d022077d4c9ffee4505
entity105 <nikchakal@inbox.ru>
2025-05-19 21:50:41 +0300

    запускается

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,17 @@
+import os
+import sys
+from pathlib import Path
+
+# Добавляем корень проекта (backend/) в sys.path
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
+
+#print("Current working directory:", os.getcwd())
+#print("sys.path:", sys.path)
+
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
